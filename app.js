9291f0ece74b571ea5e3d7d4ff0f7c52dacaec42
Marvin Juárez <25001064@falileo.edu>
2026-08-29 22:44:49 -0600

    Correcciones de Sonar

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post("/tasks", (req, res) => {
 });
 
 app.put("/tasks/:id", (req, res) => {
-    const id = parseInt(req.params.id);
+    const id = Number.parseInt(req.params.id);
     const tarea = tareas.find(t => t.id === id);
 
     if (!tarea) {
@@ -37,7 +37,7 @@ app.put("/tasks/:id", (req, res) => {
 });
 
 app.delete("/tasks/:id", (req, res) => {
-    const id = parseInt(req.params.id);
+    const id = Number.parseInt(req.params.id);
     const indice = tareas.findIndex(t => t.id === id);
 
     if (indice === -1) {
